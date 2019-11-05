90d313fcde1315971e83d6e7b96dd6f2ccf7af75
Ryan <rngao97@gmail.com>
2019-11-05 10:22:17 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,14 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        num = 0
+        songs.each do |song|
+            if song.artist_id == self.id
+                num += 1
+            end
+        end
+        return num
+    end
+    
 end
